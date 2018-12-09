066466e0fe881048bb4f5023f1688f78e0b113b8
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2018-12-09 15:23:38 +0100

    adding file read via picocli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import picocli.CommandLine;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         CommandLineApp cla = new CommandLineApp();
         new CommandLine(cla).parse(args);
--- src/main/java/commandline/CommandLineApp.java
+++ src/main/java/commandline/CommandLineApp.java
@@ -1,24 +1,44 @@
 package commandline;
 
-import picocli.CommandLine.Parameters;
-import picocli.CommandLine.Option;
+import picocli.CommandLine.*;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 
 /**
  * Created by Grzegorz Chrzaszczyk on 09-12-2018  01:32 PM
  */
 public class CommandLineApp {
 
-    @Option(names = { "-v", "--verbose" }, description = "Be verbose.")
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "display version info")
+    boolean versionInfoRequested;
+
+    @Option(names = {"-v", "--verbose"}, description = "Be verbose.")
     private boolean verbose = false;
 
-    @Parameters(arity = "1..*", paramLabel = "FILE", description = "File(s) to process.")
-    private File[] inputFiles;
+    @Parameters(index = "0", description = "File to process.")
+    private File file;
 
-    //method run command line
-    public void run(){
+    public void run() throws Exception {
 
+        if (versionInfoRequested) showVersion();
+        System.out.println("verbose = " + verbose);
 
+//        File file = new File("C:\\Users\\admin\\IdeaProjects\\txtStatistics\\test1.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        while ((st = br.readLine()) != null)
+            System.out.println(st);
     }
+
+    private static void showVersion(){
+        System.out.println("Version:1.0.0");
+    }
+
+
 }
+
+
